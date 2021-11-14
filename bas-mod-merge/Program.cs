037ea426c70c4aa391e7bf676301dcd28dce1e99
substatica <phil@substatica.com>
2021-11-14 11:21:46 -0500

    Error checking and logging added.

--- bas-mod-merge/Program.cs
+++ bas-mod-merge/Program.cs
@@ -18,30 +18,34 @@ namespace bas_mod_merge
         static string dom_jsondb_dir = temp + @"/dom_bas_jsondb";
         static string sub_jsondb_dir = temp + @"/sub_bas_jsondb";
 
-        static string output_dir = "merge_output";
+        static string output_dir = "output";
+
+        static string runlog = "";
             
         static void Main(string[] args)
         {
+            log(@"        *      *");
+            log(@"   (  (  `   (  `");
+            log(@" ( )\ )\))(  )\))(");
+            log(@" )((_|(_)()\((_)()\");
+            log(@"((_)_(_()((_|_()((_)");
+            log(@" | _ )  \/  |  \/  |");
+            log(@" | _ \ |\/| | |\/| |");
+            log(@" |___/_|  |_|_|  |_|");
+            log("");
+            log("Blade & Sorcery: Nomad Mod Merge");
+            log("");
+            log("substatica");
+            log("");
             if (args.Length < 3)
             {
-                Console.WriteLine(@"        *      *");
-                Console.WriteLine(@"   (  (  `   (  `");
-                Console.WriteLine(@" ( )\ )\))(  )\))(");
-                Console.WriteLine(@" )((_|(_)()\((_)()\");  
-                Console.WriteLine(@"((_)_(_()((_|_()((_)");
-                Console.WriteLine(@" | _ )  \/  |  \/  |");
-                Console.WriteLine(@" | _ \ |\/| | |\/| |");
-                Console.WriteLine(@" |___/_|  |_|_|  |_|");
-                Console.WriteLine();
-                Console.WriteLine("Blade & Sorcery: Nomad Mod Merge");
-                Console.WriteLine();
-                Console.WriteLine("Usage: ");
-                Console.WriteLine();
-                Console.WriteLine("  bas-mod-merge.exe [unmodified bas.jsondb] [dominant mod] [subordinate mod]");
-                Console.WriteLine();
-                Console.WriteLine("Example: ");
-                Console.WriteLine();
-                Console.WriteLine("  bas-mod-merge.exe bas.jsondb ButterStabs.zip Gravity.7z");
+                log("Usage: ");
+                log("");
+                log("  bas-mod-merge.exe [unmodified bas.jsondb] [dominant mod] [subordinate mod]");
+                log("");
+                log("Example: ");
+                log("");
+                log("  bas-mod-merge.exe bas.jsondb ButterStabs.zip Gravity.7z");
                 return;
             }
 
@@ -51,51 +55,133 @@ namespace bas_mod_merge
 
             if(!File.Exists(refFile) || !File.Exists(domFile) || !File.Exists(subFile))
             {
-                Console.WriteLine("File not found");
+                log("File not found");
                 return;
             }
 
+            log("Resetting temporary directories");
             ResetDirectories();
 
-            ExtractJsonDb(new FileInfo(domFile), dom_jsondb_dir);
-            ExtractJsonDb(new FileInfo(subFile), sub_jsondb_dir);
+            log("Preparing reference");
             ExtractJsonDb(new FileInfo(refFile), ref_jsondb_dir);
+            log("Preparing dominant mod");
+            ExtractJsonDb(new FileInfo(domFile), dom_jsondb_dir);
+            log("Preparing subordinate mod");
+            ExtractJsonDb(new FileInfo(subFile), sub_jsondb_dir);
 
-            ZipFile.ExtractToDirectory(Directory.GetFiles(ref_jsondb_dir, "*.jsondb", SearchOption.AllDirectories)[0], ref_dir);
-            ZipFile.ExtractToDirectory(Directory.GetFiles(dom_jsondb_dir, "*.jsondb", SearchOption.AllDirectories)[0], dom_dir);
-            ZipFile.ExtractToDirectory(Directory.GetFiles(sub_jsondb_dir, "*.jsondb", SearchOption.AllDirectories)[0], sub_dir);
+            log("Extracting reference");
+            try
+            {
+                ZipFile.ExtractToDirectory(Directory.GetFiles(ref_jsondb_dir, "*.jsondb", SearchOption.AllDirectories)[0], ref_dir);
+            }
+            catch (Exception ex)
+            {
+                log("Failed to extract reference");
+                throw (ex);
+            }
 
-            CompareFiles(ref_dir);
+            log("Extracting dominant mod");
+            try
+            {
+                ZipFile.ExtractToDirectory(Directory.GetFiles(dom_jsondb_dir, "*.jsondb", SearchOption.AllDirectories)[0], dom_dir);
+            }
+            catch (Exception ex)
+            {
+                log("Failed to extract dominant mod");
+                throw (ex);
+            }
+
+            log("Extracting subordinate mod");
+            try
+            {
+                ZipFile.ExtractToDirectory(Directory.GetFiles(sub_jsondb_dir, "*.jsondb", SearchOption.AllDirectories)[0], sub_dir);
+            }
+            catch (Exception ex)
+            {
+                log("Failed to extract subordinate mod");
+                throw (ex);
+            }
+
+            Int32 totalConflicts = 0;
+
+            totalConflicts += CompareFiles(ref_dir);
             foreach(string directory in Directory.GetDirectories(ref_dir))
             {
-                CompareFiles(directory);
+                totalConflicts += CompareFiles(directory);
             }
 
-            if(File.Exists(Path.Combine(output_dir, "bas.jsondb")))
+            if (File.Exists(Path.Combine(output_dir, "bas.jsondb")))
             {
+                log("Removing previous output");
                 File.Delete(Path.Combine(output_dir, "bas.jsondb"));
             }
-            ZipFile.CreateFromDirectory(mer_dir, Path.Combine(output_dir, "bas.jsondb"));
 
-            Cleanup();
+            try
+            {
+                log("Compressing merged JSON database");
+                ZipFile.CreateFromDirectory(mer_dir, Path.Combine(output_dir, "bas.jsondb"));
+            }
+            catch (Exception ex)
+            {
+                log("Failed to compress merged JSON database");
+                throw (ex);
+            }
 
-            Console.WriteLine("Merge complete.");
+            try
+            {
+                log("Removing temporary files");
+                Cleanup();
+            }
+            catch (Exception ex)
+            {
+                log("Failed to remove temporary files");
+                log("Please delete temporary directory before running again");
+                throw (ex);
+            }
+
+            log("-");
+            log( totalConflicts + " Conflicts detected");
+            log("Merge complete");
+
+            try
+            {
+                Console.WriteLine("Writing log");
+                File.WriteAllText(Path.Combine(output_dir, "output.log"), runlog);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to write log");
+                throw (ex);
+            }
+            Console.WriteLine("Complete");
+            Console.WriteLine("");
         }
 
-        static void CompareFiles(string directory) {
+        static void log(string line)
+        {
+            Console.WriteLine(line);
+            runlog += line + Environment.NewLine;
+        }
+
+        static int CompareFiles(string directory) {
             DirectoryInfo dirInfo = new DirectoryInfo(directory);
             DirectoryInfo merInfo = directory == ref_dir ? new DirectoryInfo(mer_dir) : Directory.CreateDirectory(Path.Combine(mer_dir, dirInfo.Name));
 
+            Int32 conflictCount = 0;
+            
             foreach (string file in Directory.GetFiles(directory))
             {
                 FileInfo fileInfo = new FileInfo(file);
+                Boolean subChange = false;
+                Boolean domChange = false;
 
                 if (File.Exists(Path.Combine(sub_dir, dirInfo.Name, fileInfo.Name)))
                 {
                     FileInfo subInfo = new FileInfo(Path.Combine(sub_dir, dirInfo.Name, fileInfo.Name));
                     if (!FilesAreEqual(fileInfo, subInfo))
                     {
-                        Console.WriteLine("Subordinate Mod: " + subInfo);
+                        subChange = true;
+                        log("Subordinate mod - " + subInfo);
                         subInfo.CopyTo(Path.Combine(merInfo.FullName, subInfo.Name));
                     }
                 }
@@ -105,7 +191,8 @@ namespace bas_mod_merge
                     FileInfo domInfo = new FileInfo(Path.Combine(dom_dir, dirInfo.Name, fileInfo.Name));
                     if (!FilesAreEqual(fileInfo, domInfo))
                     {
-                        Console.WriteLine("Dominant Mod: " + domInfo);
+                        domChange = true;
+                        log("Dominant mod - " + domInfo);
                         domInfo.CopyTo(Path.Combine(merInfo.FullName, domInfo.Name), true);
                     }
                 }
@@ -114,7 +201,16 @@ namespace bas_mod_merge
                 {
                     fileInfo.CopyTo(Path.Combine(merInfo.FullName, fileInfo.Name), true);
                 }
+
+                if (domChange && subChange)
+                {
+                    log("^");
+                    log("Conflict detected");
+                    log("-");
+                    conflictCount++;
+                }
             }
+            return conflictCount;
         }
 
         static void Cleanup()
@@ -142,8 +238,8 @@ namespace bas_mod_merge
             }
             catch(Exception ex)
             {
-                Console.WriteLine("Could not reset temporary directories. Please close all directories.");
-                Console.WriteLine("If you continue to experience this issue manually delete the '"+temp+"' directory.");
+                log("Could not reset temporary directories. Please close all directories.");
+                log("If you continue to experience this issue manually delete the '"+temp+"' directory.");
                 throw (ex);
             }
     }
@@ -185,13 +281,17 @@ namespace bas_mod_merge
                 case ".7z":
                 case ".zip":
                 case ".rar":
+                    log("Archive detected");
+                    log("Extracting to " + dir);
                     ExtractFile(fi.FullName, dir);
                     break;
                 case ".jsondb":
+                    log("JSON database detected");
+                    log("Copying to " + dir);
                     File.Copy(fi.FullName, Path.Combine(dir, fi.Name));
                     break;
                 default:
-                    Console.WriteLine("Unsupported file type detected");
+                    log("Unsupported file type detected");
                     throw (new Exception("Unsupported file type detected"));
             }
         }
